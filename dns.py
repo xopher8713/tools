ddab9714da4a9a3bd3b1bf9effb8af1b99c08bf8
xopher8713 <xopher8713@gmail.com>
2021-03-14 13:51:38 -0500

    Adding dns.py
    dns.py gives an IP address for a specific hostname, dns servers can be changed and I used 1.1.1.1 for the example

---
+++ dns.py
@@ -0,0 +1,28 @@
+import sys
+import dnspython3 as dns
+import dnspython3.resolver
+import dnspython3.exception
+
+def dnsquery(hostname):
+
+    resolve = dnspython3.resolver.Resolver()
+    resolve.nameservers = ['1.1.1.1', '1.0.0.1']
+    try:
+        response = resolve.resolve(hostname)
+        for rdata in response:
+            ipaddress = rdata.address
+            return ipaddress
+    except dns.exception.DNSException:
+        print("Unable to resolve DNS, no entries found.")
+    except Exception as e:
+        print("Unable to resolve DNS for {} due to the following error, {}.".format(hostname, e))
+
+if __name__ in '__main__':
+    try:
+        hostname = sys.argv[1]
+        ipaddress = dnsquery(hostname)
+        print("IP address {} returned for hostname {}.".format(ipaddress, hostname))
+    except Exception as e:
+        print("Unable to resolve DNS for {} due to the following error, {}.".format(hostname, e))
+    except KeyboardInterrupt:
+        print("\nProcess interupted by user. Exiting...")
